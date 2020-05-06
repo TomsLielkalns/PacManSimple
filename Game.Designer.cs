02f2de0a71a5e48628f9b81f2b69a2714f18a7f5
TomsLielkalns <tomslielkalns@gmail.com>
2020-05-06 18:24:15 +0300

    Add project files.

---
+++ Game.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace PacManSimple
+{
+    partial class Game
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Game));
+            this.Hero = new System.Windows.Forms.PictureBox();
+            this.Food = new System.Windows.Forms.PictureBox();
+            this.Enemy = new System.Windows.Forms.PictureBox();
+            this.TimerMove = new System.Windows.Forms.Timer(this.components);
+            this.TimerAnimate = new System.Windows.Forms.Timer(this.components);
+            this.ScoreLabel = new System.Windows.Forms.Label();
+            this.TimerHeroMelt = new System.Windows.Forms.Timer(this.components);
+            this.LabelGameOver = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.Hero)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Food)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Enemy)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Hero
+            // 
+            this.Hero.Location = new System.Drawing.Point(86, 120);
+            this.Hero.Name = "Hero";
+            this.Hero.Size = new System.Drawing.Size(150, 150);
+            this.Hero.TabIndex = 1;
+            this.Hero.TabStop = false;
+            // 
+            // Food
+            // 
+            this.Food.Location = new System.Drawing.Point(253, 274);
+            this.Food.Name = "Food";
+            this.Food.Size = new System.Drawing.Size(50, 50);
+            this.Food.TabIndex = 2;
+            this.Food.TabStop = false;
+            // 
+            // Enemy
+            // 
+            this.Enemy.Location = new System.Drawing.Point(565, 130);
+            this.Enemy.Name = "Enemy";
+            this.Enemy.Size = new System.Drawing.Size(80, 80);
+            this.Enemy.TabIndex = 3;
+            this.Enemy.TabStop = false;
+            // 
+            // TimerMove
+            // 
+            this.TimerMove.Interval = 20;
+            this.TimerMove.Tick += new System.EventHandler(this.TimerMove_Tick);
+            // 
+            // TimerAnimate
+            // 
+            this.TimerAnimate.Tick += new System.EventHandler(this.TimerAnimate_Tick);
+            // 
+            // ScoreLabel
+            // 
+            this.ScoreLabel.AutoSize = true;
+            this.ScoreLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ScoreLabel.Location = new System.Drawing.Point(12, 9);
+            this.ScoreLabel.Name = "ScoreLabel";
+            this.ScoreLabel.Size = new System.Drawing.Size(79, 29);
+            this.ScoreLabel.TabIndex = 4;
+            this.ScoreLabel.Text = "label1";
+            // 
+            // TimerHeroMelt
+            // 
+            this.TimerHeroMelt.Interval = 150;
+            this.TimerHeroMelt.Tick += new System.EventHandler(this.TimerHeroMelt_Tick);
+            // 
+            // LabelGameOver
+            // 
+            this.LabelGameOver.AutoSize = true;
+            this.LabelGameOver.BackColor = System.Drawing.Color.Transparent;
+            this.LabelGameOver.Font = new System.Drawing.Font("Script MT Bold", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LabelGameOver.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.LabelGameOver.Location = new System.Drawing.Point(262, 152);
+            this.LabelGameOver.Name = "LabelGameOver";
+            this.LabelGameOver.Size = new System.Drawing.Size(232, 58);
+            this.LabelGameOver.TabIndex = 5;
+            this.LabelGameOver.Text = "Game Over";
+            this.LabelGameOver.Visible = false;
+            // 
+            // Game
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.LabelGameOver);
+            this.Controls.Add(this.ScoreLabel);
+            this.Controls.Add(this.Enemy);
+            this.Controls.Add(this.Food);
+            this.Controls.Add(this.Hero);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Game";
+            this.Text = "Datorium Bootleg Pac-Man";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Game_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.Hero)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Food)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Enemy)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox Hero;
+        private System.Windows.Forms.PictureBox Food;
+        private System.Windows.Forms.PictureBox Enemy;
+        private System.Windows.Forms.Timer TimerMove;
+        private System.Windows.Forms.Timer TimerAnimate;
+        private System.Windows.Forms.Label ScoreLabel;
+        private System.Windows.Forms.Timer TimerHeroMelt;
+        private System.Windows.Forms.Label LabelGameOver;
+    }
+}
+
